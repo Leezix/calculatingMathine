15b82d9de99d08d58b02ce954450c2fb1020ef24
李主贤 <lizhuxian020@pingan.com.cn>
2017-03-21 22:58:41 +0800

    完成计算机UI

---
+++ calculatingMachine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calculatingMachine
+//
+//  Created by lzx on 17/3/21.
+//  Copyright © 2017年 lzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
